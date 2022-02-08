228539fc0d52966abf422879d6fb6d4dce6cdb79
Htin Wana <htinwana.dev@gmail.com>
2022-02-08 10:52:58 +0630

    Format file

--- src/axios-global.js
+++ src/axios-global.js
@@ -1,12 +1,10 @@
-import axios from "axios";
+import axios from 'axios';
 
 export const axiosGloabl = axios.create({
-    //local
-    baseURL: `http://192.168.100.4:3000`,
-    headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-    },
+  //local
+  baseURL: `http://192.168.100.4:3001`,
+  headers: {
+    Accept: 'application/json',
+    'Content-Type': 'application/json',
+  },
 });
-
-
