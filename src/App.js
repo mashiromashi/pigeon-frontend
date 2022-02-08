61b98dcbe6e2fcbe298b99975f1c29b14faf2180
Htin Wana <htinwana.dev@gmail.com>
2022-02-08 10:52:48 +0630

    Format File

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import { ThemeProvider } from "@mui/system";
-import { useEffect } from "react";
-import MainRoute from "router/MainRoute";
-import { theme } from "theme/theme";
-import { storage } from "util/storage";
-import { useDispatch } from "react-redux";
-import { logout } from "applications/slice/loginSlice";
-import { useNavigate } from "react-router-dom";
-import { axiosGloabl } from "axios-global";
-import "./styles/styles.css"
+import { ThemeProvider } from '@mui/system';
+import { useEffect } from 'react';
+import MainRoute from 'router/MainRoute';
+import { theme } from 'theme/theme';
+import { storage } from 'util/storage';
+import { useDispatch } from 'react-redux';
+import { logout } from 'applications/slice/loginSlice';
+import { useNavigate } from 'react-router-dom';
+import { axiosGloabl } from 'axios-global';
+import './styles/styles.css';
 
 const App = () => {
   const loginUser = storage.getUserEntity();
@@ -15,15 +15,15 @@ const App = () => {
   const navigate = useNavigate();
 
   if (loginUser?.accessToken) {
-    axiosGloabl.defaults.headers.common["Authentication"] = "Bearer " + loginUser?.accessToken;
+    axiosGloabl.defaults.headers.common['Authentication'] =
+      'Bearer ' + loginUser?.accessToken;
   }
-
 
   useEffect(() => {
     if (loginUser?.accessToken) {
       setTimeout(() => {
         dispatch(logout());
-        navigate("/login");
+        navigate('/login');
       }, 72000 * 1000);
     }
   }, [loginUser?.accessToken, dispatch, navigate]);
